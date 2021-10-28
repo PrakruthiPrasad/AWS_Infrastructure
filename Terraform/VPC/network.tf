467cbd33ae0a508c1c52bca9c5035ce0d6880c8c
PrakruthiPrasad <bagursuryanarayana.p@northeastern.edu>
2021-10-28 16:19:49 -0400

    committing assignment4 changes

--- Terraform/VPC/network.tf
+++ Terraform/VPC/network.tf
@@ -13,16 +13,117 @@ resource "aws_route_table" "public_crt" {
 
     gateway_id = aws_internet_gateway.igw.id
   }
+  timeouts {
+    create = "5m"
+  }
 
   tags = {
     Name = var.public_rt
   }
 }
 
-resource "aws_route_table_association" "crta_public_subnet" {
+// resource "aws_route_table_association" "crta_public_subnet" {
 
-  for_each = local.subnet_az_cidr
+//   for_each = local.subnet_az_cidr
 
-  subnet_id      = aws_subnet.subnet[each.key].id
+//   subnet_id      = aws_subnet.subnet[each.key].id
+//   route_table_id = aws_route_table.public_crt.id
+// }
+
+resource "aws_route_table_association" "crta_public_subnet1" {
+
+  //depends_on = [aws_vpc.subnet1]
+  subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.public_crt.id
+}
+
+resource "aws_route_table_association" "crta_public_subnet2" {
+
+  //depends_on = [aws_vpc.subnet2]
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_crt.id
+}
+
+resource "aws_route_table_association" "crta_public_subnet3" {
+
+  //depends_on = [aws_vpc.subnet3]
+  subnet_id      = aws_subnet.subnet3.id
+  route_table_id = aws_route_table.public_crt.id
+}
+
+
+resource "aws_security_group" "application_security_group" {
+  depends_on = [aws_vpc.vpc]
+  vpc_id     = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = var.protocol
+    description = "PORT 22"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  ingress {
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = var.protocol
+    description = "PORT 80"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  ingress {
+    from_port   = var.https_port
+    to_port     = var.https_port
+    protocol    = var.protocol
+    description = "PORT 443"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  ingress {
+    from_port   = var.custom_tcp_port
+    to_port     = var.custom_tcp_port
+    protocol    = var.protocol
+    description = "PORT 8080"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  tags = {
+    Name = var.SG_name
+  }
+}
+
+
+resource "aws_security_group" "database_security_group" {
+  depends_on = [aws_vpc.vpc]
+
+  vpc_id      = aws_vpc.vpc.id
+  description = "Allow application traffic"
+
+  ingress {
+    from_port       = var.http_port
+    to_port         = var.db_port
+    protocol        = var.protocol
+    description     = "PORT 3306"
+    security_groups = ["${aws_security_group.application_security_group.id}"]
+    cidr_blocks     = [var.dest_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.dest_cidr_block]
+  }
+
+  tags = {
+    Name = var.db_sg_name
+  }
 }
--- Terraform/VPC/variables.tf
+++ Terraform/VPC/variables.tf
@@ -26,8 +26,6 @@ variable "dest_cidr_block" {
   type        = string
   description = "destination cidr block"
 }
-
-
 variable "subnet_name" {
   type        = string
   description = "name of subnet"
@@ -49,3 +47,95 @@ variable "profileName" {
   type        = string
   description = "Profile name"
 }
+
+variable "subnetGroup" {
+  type        = string
+  description = "RDS subnet group"
+}
+
+
+//Variables for Security group
+variable "SG_name" {
+  type        = string
+  description = "Security group name"
+}
+
+variable "db_sg_name" {
+  type        = string
+  description = "Security group name"
+}
+
+variable "protocol" {
+  type        = string
+  description = "Protocol for the security group"
+}
+
+variable "ssh_port" {
+  type        = number
+  description = "ssh port number"
+}
+
+variable "http_port" {
+  type        = number
+  description = "http port number"
+}
+
+variable "https_port" {
+  type        = number
+  description = "https port number"
+}
+
+variable "custom_tcp_port" {
+  type        = number
+  description = "custom tcp port number"
+}
+
+variable "db_port" {
+  type        = number
+  description = "db port number"
+}
+
+//Variables for RDS instance
+variable "identifier" { type = string }
+variable "allocated_storage" { type = number }
+variable "multi_az" { type = bool }
+variable "engine" { type = string }
+variable "engine_version" { type = string }
+variable "instance_class" { type = string }
+variable "database_name" { type = string }
+variable "username" { type = string }
+variable "password" { type = string }
+variable "db_subnet_group_name" { type = string }
+variable "skip_final_snapshot" { type = bool }
+variable "final_snapshot_identifier" { type = string }
+variable "publicly_accessible" { type = bool }
+
+
+//Variables for RDS parameter group
+variable "name" { type = string }
+variable "family" { type = string }
+variable "parameter1_name" { type = string }
+variable "value1" { type = string }
+variable "parameter2_name" { type = string }
+variable "value2" { type = string }
+variable "apply_method" { type = string }
+
+
+//Variables for EC2 instance
+variable "ami" { type = string }
+variable "instance_type" { type = string }
+variable "disable_api_termination" { type = bool }
+variable "user_data" { type = string }
+variable "device_name" { type = string }
+variable "volume_size" { type = number }
+variable "volume_type" { type = string }
+variable "key_name" { type = string }
+variable "PUBLIC_KEY_PATH" { type = string }
+variable "ec2name" { type = string }
+
+//Variables for IAM roles and policy
+variable "IAMRoleName" { type = string }
+variable "IAMRoleProfile" { type = string }
+variable "bucketname" { type = string }
+variable "envName" { type = string }
+variable "iam_policy_attach" { type = string }
